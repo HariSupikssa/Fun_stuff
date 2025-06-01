2c4381a472d4b5eb59c27807fede59b6f43c5102
HariSupikssa <131352388+HariSupikssa@users.noreply.github.com>
2025-06-01 13:38:37 +0530

    add preload script to main.js

--- my-electron-app/main.js
+++ my-electron-app/main.js
@@ -4,7 +4,10 @@ import { app, BrowserWindow } from "electron"
 const createWindow = ()=>{
     const win = new BrowserWindow({
         width : 800,
-        height: 600
+        height: 600,
+        webPreferences:{
+            preload: Path2D.join(__dirname, 'preload.js')
+        }
     })
 
     win.loadFile('index.html')
