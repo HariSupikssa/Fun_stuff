7eb5a358fe48ed4b3856431e14b991b4bbfb579e
HariSupikssa <131352388+HariSupikssa@users.noreply.github.com>
2025-06-03 14:14:29 +0530

    preserve practice code for reference

--- my-electron-app/main.js
+++ my-electron-app/main.js
@@ -3,14 +3,23 @@ const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const createWindow = () => {
     const win = new BrowserWindow({
+        // // Showing the window gracefully
+        // show: false,
         width: 800,
         height: 600,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
+       
     })
-
-    win.loadFile('index.html')
+     
+    // // Showing the window gracefully
+    // win.loadFile('index.html')
+    // win.once('ready-to-show',()=>{
+    //         win.show();
+    //     })
+    // win.loadURL('https://soundcloud.com/adelynnxx/ounder-the-influence-x-i-was-6');
+    
 }
 
 app.whenReady().then(() => {
