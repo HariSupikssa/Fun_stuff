11a1a88c027d8b317cd4be672c1fe3aea6d59369
HariSupikssa <131352388+HariSupikssa@users.noreply.github.com>
2025-06-09 19:21:20 +0530

    make app draggable

--- my-electron-app/function.js
+++ my-electron-app/function.js
@@ -1,5 +1,6 @@
 const {ipcRenderer} = require('electron')
 
 document.querySelector('#closeBtn').addEventListener('click',()=>{
+    console.log("Close button clicked");
     ipcRenderer.send('closeApp')
 })
--- my-electron-app/preload.js
+++ my-electron-app/preload.js
@@ -1,8 +1,9 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('versions', {
-    node: () => process.versions.node,
-    chrome: () => process.versions.chrome,
-    // electron: () => process.versions.electron
-    "electron": process.versions.electron
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  // electron: () => process.versions.electron
+  "electron": process.versions.electron,
 })
+
