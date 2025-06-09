08a391f4bd7dcf82fa9cd34484de2e28f6c2c589
HariSupikssa <131352388+HariSupikssa@users.noreply.github.com>
2025-06-09 19:33:43 +0530

    structure closeBtn

--- my-electron-app/renderer.js
+++ my-electron-app/renderer.js
@@ -1,5 +1,6 @@
-const information = document.getElementById('info')
-information.innerText = `Chrome(v${versions.chrome()}), Node.js (v${versions.node()}) and Electron (v${versions.electron}) `
+// const information = document.getElementById('info')
+// information.innerText = `Chrome(v${versions.chrome()}), Node.js (v${versions.node()}) and Electron (v${versions.electron}) `
 
+// // since preload.js is commented out in main.js, and the variables come from preload.js, this is of no use anymore
 
 
