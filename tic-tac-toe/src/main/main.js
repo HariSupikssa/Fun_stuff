3ff132cf1dfb1d300bf8a04674db92a10b4deaef
HariSupikssa <131352388+HariSupikssa@users.noreply.github.com>
2025-06-16 09:45:36 +0530

    Load webpage

--- tic-tac-toe/src/main/main.js
+++ tic-tac-toe/src/main/main.js
@@ -1,1 +1,14 @@
-console.log("Hello, World!")
+// console.log("Hello, World!")
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 400,
+        height: 500,
+    })
+    win.loadFile('../website/index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
