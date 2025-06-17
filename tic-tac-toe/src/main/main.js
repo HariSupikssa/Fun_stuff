f8f8a2e8d00583df4cd91cde4f9099de9689f0cd
HariSupikssa <131352388+HariSupikssa@users.noreply.github.com>
2025-06-17 14:09:16 +0530

    add the grid for game

--- tic-tac-toe/src/main/main.js
+++ tic-tac-toe/src/main/main.js
@@ -4,7 +4,7 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 400,
-        height: 500,
+        height: 400,
         resizable: false,
         frame: false,
     })
