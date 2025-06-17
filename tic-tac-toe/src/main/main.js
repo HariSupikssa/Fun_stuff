1e346485c08359ad0a22a044e837a5973fece080
HariSupikssa <131352388+HariSupikssa@users.noreply.github.com>
2025-06-17 13:01:24 +0530

    add closeBtn, stylesheet

--- tic-tac-toe/src/main/main.js
+++ tic-tac-toe/src/main/main.js
@@ -5,7 +5,10 @@ const createWindow = () => {
     const win = new BrowserWindow({
         width: 400,
         height: 500,
+        resizable: false,
+        frame: false,
     })
+    
     win.loadFile('../website/index.html')
 }
 
