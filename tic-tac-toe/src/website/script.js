04b5469463b6196594bc886bb0ecc0d87fb41609
HariSupikssa <131352388+HariSupikssa@users.noreply.github.com>
2025-06-19 17:53:04 +0530

    add checkWinner function

--- tic-tac-toe/src/website/script.js
+++ tic-tac-toe/src/website/script.js
@@ -1,12 +1,12 @@
 const winPatterns = [
-  [0, 1, 2],
-  [0, 3, 6],
-  [0, 4, 8],
-  [1, 4, 7],
-  [2, 5, 8],
-  [2, 4, 6],
-  [3, 4, 5],
-  [6, 7, 8]
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
 ];
 
 let turnO = true
@@ -22,14 +22,14 @@ let boxes = document.querySelectorAll('.box')
 let restartBtn = document.querySelector('#restartBtn')
 
 
-boxes.forEach((box)=>{
-    box.addEventListener('click',()=>{
-        if(box.dataset.clicked === "true" || gameOver) return
+boxes.forEach((box) => {
+    box.addEventListener('click', () => {
+        if (box.dataset.clicked === "true" || gameOver) return
 
         box.dataset.clicked = "true"
 
         const img = document.createElement('img')
-        let source = turnO? bomb: sword
+        let source = turnO ? bomb : sword
         img.src = source
         img.style.width = "95%"
         img.style.height = "95%"
@@ -38,5 +38,36 @@ boxes.forEach((box)=>{
 
         turnO = !turnO
         moveCount++
+        checkWinner()
     })
 })
+
+const checkWinner = () => {
+
+    for (let pattern of winPatterns) {
+        let img1 = boxes[pattern[0]].querySelector("img");
+        let img2 = boxes[pattern[1]].querySelector("img");
+        let img3 = boxes[pattern[2]].querySelector("img");
+        // gives <img>
+
+        let val1 = img1 ? img1.src : null;
+        let val2 = img2 ? img2.src : null;
+        let val3 = img3 ? img3.src : null;
+        // checks if <img> exists, take src else null
+
+        if (val1 !== null && val2 !== null && val3 !== null && val1 === val2 && val2 === val3) {
+            hasWin = true
+            gameOver = true
+
+            let winner = val1 === bomb ? "O" : "X"
+            return
+
+
+        }
+    }
+    if (!hasWin && moveCount === 9) {
+        msg.innerHTML = "It's a draw!"
+        msgCon.classList.remove("hide")
+        gameOver = true
+    }
+}
