480181b0b8910012a57904c4d18aaded61f6d112
HariSupikssa <131352388+HariSupikssa@users.noreply.github.com>
2025-06-20 19:58:16 +0530

    adjust volume

--- tic-tac-toe/src/main/main.js
+++ tic-tac-toe/src/main/main.js
@@ -14,7 +14,7 @@ const createWindow = () => {
         {
             nodeIntegration: true,
             contextIsolation: false,
-            // devTools: true,
+            devTools: true,
 
         }
     })
